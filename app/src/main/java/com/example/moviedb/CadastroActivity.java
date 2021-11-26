b3a4f7dec1b54848ab59830fa48c0542fbe68a17
Matheus Neri <matheus.neri.del@gmail.com>
2021-11-25 23:11:09 -0400

    cadastro pronto

--- app/src/main/java/com/example/moviedb/CadastroActivity.java
+++ app/src/main/java/com/example/moviedb/CadastroActivity.java
@@ -3,12 +3,65 @@ package com.example.moviedb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.moviedb.model.DaoMaster;
+import com.example.moviedb.model.DaoSession;
+import com.example.moviedb.model.User;
+import com.example.moviedb.model.UserDao;
+
+import org.greenrobot.greendao.database.Database;
 
 public class CadastroActivity extends AppCompatActivity {
+    private EditText textEmail;
+    private EditText textPassword;
+    private EditText textPasswordConfirm;
+    private EditText textApelido;
+    private EditText textDescricao;
+    private Button cadastrarBtn;
+
+    private UserDao userDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        textEmail = findViewById(R.id.textEmailCadastro);
+        textPassword = findViewById(R.id.textSenhaCadastro);
+        textPasswordConfirm = findViewById(R.id.textSenhaConfirm);
+        textApelido = findViewById(R.id.textApelido);
+        textDescricao = findViewById(R.id.textDescricao);
+        cadastrarBtn = findViewById(R.id.cadastrarBtn);
+
+        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "notes-db");
+        Database db = helper.getWritableDb();
+        DaoSession daoSession = new DaoMaster(db).newSession();
+
+        userDao = daoSession.getUserDao();
+    }
+
+    private void cadastrar(View view){
+        String password = textPassword.getText().toString();
+        String username = textEmail.getText().toString();
+        String nickName = textApelido.getText().toString();
+        String description = textDescricao.getText().toString();
+
+        if (password != textPasswordConfirm.getText().toString()) {
+            Toast.makeText(this, "Erro ao confirmar a senha",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        User user = new User();
+        user.setUserName(username);
+        user.setDescription(description);
+        user.setPassword(password);
+        user.setNickName(nickName);
+
+        userDao.insert(user);
+        Toast.makeText(this, "Novo usuario"+String.valueOf(user.getId()),Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/moviedb/MainActivity.java
+++ app/src/main/java/com/example/moviedb/MainActivity.java
@@ -30,100 +30,52 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText textEmail;
+    private EditText textPassword;
+    private Button loginBtn;
+    private Button cadastroBtn;
+    private Button semLoginBtn;
 
     private UserDao userDao;
-
-    private EditText PTPassword;
-    private EditText PTEmail;
-    private EditText PTNickName;
-    private EditText PTDescription;
-
-    private TextView test;
-
-    private JsonPlaceHolderApi jsonPlaceHolderApi;
-
-    private Button ButtonAdd;
-    private Button API;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textEmail = findViewById(R.id.textEmailLogin);
+        textPassword = findViewById(R.id.textSenhaLogin);
+        loginBtn = findViewById(R.id.loginBtn);
+        cadastroBtn = findViewById(R.id.cadastroBtn);
+        semLoginBtn = findViewById(R.id.semLoginBtn);
+
         DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "notes-db");
         Database db = helper.getWritableDb();
         DaoSession daoSession = new DaoMaster(db).newSession();
 
         userDao = daoSession.getUserDao();
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://api.themoviedb.org/3/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
-
-
     }
 
 
 
-    public void addButton(View view){
-        addUser();
+    public void login(View view){
+        List<User> queryResult = userDao.queryRaw(
+                "userName=? and password=?", textEmail.getText().toString());
+
+        if (queryResult.isEmpty()) {
+            Toast.makeText(this, "Usuário ou senha incorretos",Toast.LENGTH_LONG).show();
+            return;
+        }
+        // TODO
     }
 
-    public void fetch(View view){
-        fetchData();
+    public void cadastro(View view){
+        // TODO
     }
 
-    private void fetchData(){
-        Map<String, String> data = new HashMap<>();
-        data.put("api_key", "bb0e5cab69c4b3502ab1a75cd9c7f371");
-        data.put("language", "pt-BR");
-
-
-        //E aqui que deve ser alterado para pegar do campo de pesquisa. "aranha" -> TextField.getText.
-        data.put("query", "aranha");
-
-        Call<Test> call = jsonPlaceHolderApi.getPost(data);
-        call.enqueue(new Callback<Test>() {
-            @Override
-            public void onResponse(Call<Test> call, Response<Test> response) {
-                if(!response.isSuccessful()){
-                    test.setText("Code: " + response.code());
-                    return;
-                }else{
-                 List<Result> teste = response.body().getResults();
-
-                    for(Result result : teste){
-                        String content = "";
-                        content+= "title" + result.getOriginalTitle();
-                        content+= "\n vote average " + result.getVoteAverage();
-                        content+= "x\n\n";
-                        test.append(content);
-                    }
-                }
-            }
-            @Override
-            public void onFailure(Call<Test> call, Throwable t) {
-                test.setText("ERROR!!"+t.getMessage());
-
-            }
-        });
+    public void semLogin(View view){
+        // TODO
     }
 
-    private void addUser(){
-        String password = PTPassword.getText().toString();
-        String username = PTEmail.getText().toString();
-        String nickName = PTNickName.getText().toString();
-        String description = PTDescription.getText().toString();
 
-        User user = new User();
-        user.setUserName(username);
-        user.setDescription(description);
-        user.setPassword(password);
-        user.setNickName(nickName);
-
-        userDao.insert(user);
-        Toast.makeText(this, "Novo usuario"+String.valueOf(user.getId()),Toast.LENGTH_SHORT).show();
-    }
 }
