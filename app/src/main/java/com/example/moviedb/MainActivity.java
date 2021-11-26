d4f77d2369049d70528762147ce70e2de827291b
Matheus Neri <matheus.neri.del@gmail.com>
2021-11-25 23:39:07 -0400

    cadastro pronto

--- app/src/main/java/com/example/moviedb/MainActivity.java
+++ app/src/main/java/com/example/moviedb/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.moviedb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -70,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void cadastro(View view){
-        // TODO
+        startActivity(new Intent(MainActivity.this, CadastroActivity.class));
     }
 
     public void semLogin(View view){
